c31dfc25d34c9c2735b0d1cd4b87d7d4e8e7b3d0
Khanh Duong <verykhanhny@gmail.com>
2024-04-09 13:57:08 -0600

    revert middleware

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {
   InteractionType,
   InteractionResponseType,
-  verifyKeyMiddleware,
 } from 'discord-interactions';
 import { GetConfig, DiscordRequest, VerifyDiscordRequest, GetAiClient, getRandomEmoji } from './utils.js';
 
@@ -60,8 +59,6 @@ let conversation = [
 
 // Get port
 const PORT = 11111;
-// Parse request body and verifies incoming requests using discord-interactions package
-// app.use(express.json({ verify: VerifyDiscordRequest(config.PUBLIC_KEY) }));
 
 // Health check
 app.get('/', (req, res) => {
@@ -72,7 +69,7 @@ app.get('/', (req, res) => {
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', verifyKeyMiddleware(config.PUBLIC_KEY), handleInteractions);
+app.post('/interactions', express.json({ verify: VerifyDiscordRequest(config.PUBLIC_KEY) }), handleInteractions);
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
